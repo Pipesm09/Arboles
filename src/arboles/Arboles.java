b6809861c54a960495e20c893b46dbf30929db35
Pipesm09 <juansafe2@gmail.com>
2026-04-24 12:56:38 -0500

    xd

--- src/arboles/Arboles.java
+++ src/arboles/Arboles.java
@@ -1,5 +1,7 @@
 package arboles;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ASUS
@@ -10,4 +12,21 @@ public class Arboles {
         
     }
     
+    public static int Menu(){
+        int opc = Integer.parseInt(JOptionPane.showInputDialog("****** Menu Principal ******" 
+                + "1. Recprrodp InOrden."
+                + "2. Recorrido PreOrden"
+                + "3. Recorrido PosOrden"
+                + "4. Eliminar Termino"
+                + "5. Mostrar Arbol"
+                + "0. Salir."
+                + "Ingrese una opcion"));
+        
+    return opc;
+    }
+    
+    public static void IngresarArbol(){
+        JOptionPane.showInputDialog("Ingrese la cadena del arbol");
+        char Vc[];
+    }
 }
